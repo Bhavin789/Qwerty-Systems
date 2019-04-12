ef51f5104bf54d26d36e19b4d15d1a3e81f14a34
Bhavin Agarwal <elnino@ElniNo.local>
2019-04-12 22:30:05 +0530

    Add TableViewCell for videos, WIP

---
+++ Qwerty Systems/View/VideoInfoTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  VideoInfoTableViewCell.swift
+//  Qwerty Systems
+//
+//  Created by Bhavin Agarwal on 12/04/19.
+//  Copyright © 2019 Qwerty System. All rights reserved.
+//
+
+import UIKit
+
+class VideoInfoTableViewCell: UITableViewCell {
+    
+    let videoImage: UIImageView = {
+        let videoImage = UIImageView()
+        videoImage.image = UIImage(named: "initialProfileImage")
+        videoImage.translatesAutoresizingMaskIntoConstraints = false
+        videoImage.layer.cornerRadius = 20
+        videoImage.layer.masksToBounds = true
+        return videoImage
+    }()
+    
+    private func setupTitleAndVideoImage(){}
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 56, y: (textLabel?.frame.origin.y)!, width: (textLabel?.frame.width)!, height: (textLabel?.frame.height)!)
+        
+        detailTextLabel?.frame = CGRect(x: 56, y: (detailTextLabel?.frame.origin.y)!, width: (detailTextLabel?.frame.width)!, height: (detailTextLabel?.frame.height)!)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(videoImage)
+        
+        videoImage.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        videoImage.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        videoImage.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        videoImage.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
