d4f0c02e0fddf49850fc4581bb55f5f024c5064d
Bhavin Agarwal <elnino@ElniNo.local>
2019-04-13 00:59:45 +0530

    Register cell to table view controller

--- Qwerty Systems/Controller/VideoListTableViewController.swift
+++ Qwerty Systems/Controller/VideoListTableViewController.swift
@@ -9,38 +9,54 @@
 import UIKit
 
 class VideoListTableViewController: UITableViewController {
+    
+    private var videoItems = [VideoItem]()
+    
+    private let cellId = "videoCellId"
+    
+    func fetchVideoList(){
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        tableView.register(VideoInfoTableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        fetchVideoList()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return videoItems.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? VideoInfoTableViewCell
+        
+        let videoItem = videoItems[indexPath.row]
+        cell?.videoItem = videoItem
         // Configure the cell...
 
-        return cell
+        return cell!
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let videoItem = videoItems[indexPath.row]
+    }
+ 
 
     /*
     // Override to support conditional editing of the table view.
